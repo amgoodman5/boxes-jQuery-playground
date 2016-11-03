288a8ee640109bc73538571085f2fb9d20f21011
Aaron Goodman <Amgoodman5@Aarons-MacBook-Air.local>
2016-11-03 15:51:56 -0600

    changing css classes

--- boxes.js
+++ boxes.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+$(document).ready(function() {
+    $("#secretBox").css('background-color', 'white').append('<h1>secret box!</h1>')
+
+    $('#row1').children().addClass('boxType3');
+
+    $('#row4 > div:last-child').css('display', "none");
+
+    $('.boxType1').css('background-color', 'white');
+
+    $('#row2 > div:nth-child(1), #row2 > div:nth-child(2)').removeClass();
+    $('.box').not('#secretBox').css('width', '2px');
+
+});
